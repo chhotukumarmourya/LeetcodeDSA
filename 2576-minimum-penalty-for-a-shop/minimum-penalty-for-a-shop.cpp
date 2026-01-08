f73bde72dad3d5d2fdb0ddfa9bbed94edf6f507e
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-01-08 17:10:49 +0530

    Time: 11 ms (23.90%) | Memory: 21.9 MB (5.00%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int n = customers.length();
-
         vector<int> pre(n + 1, 0);
         vector<int> suf(n + 1, 0);
-
-        // prefix: N before i
-        for (int i = 0; i < n; i++) {
-            pre[i + 1] = pre[i] + (customers[i] == 'N');
+         pre[0]=0;
+        for(int i=0;i<n;i++){
+            pre[i+1]=pre[i]+((customers[i]=='N')?1:0);
         }
-
-        // suffix: Y after i
-        for (int i = n - 1; i >= 0; i--) {
-            suf[i] = suf[i + 1] + (customers[i] == 'Y');
+        for(int i=n-1;i>=0;i--){
+            suf[i]=suf[i+1]+((customers[i]=='Y')?1:0);
         }
-
-        int minPenalty = INT_MAX;
-        int res = 0;
-
-        for (int i = 0; i <= n; i++) {
-            int penalty = pre[i] + suf[i];
-            if (penalty < minPenalty) {
-                minPenalty = penalty;
-                res = i;
-            }
+        int minimum=INT_MAX;
+        for(int i=0;i<=n;i++){
+            
+            int pen=pre[i]+suf[i];
+            minimum=min(minimum,pen);
         }
-
-        return res;
+        for(int i=0;i<=n;i++){
+            if(pre[i]+suf[i]==minimum)return i;
+        }
+        return n;
     }
 };
