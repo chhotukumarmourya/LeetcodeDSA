4ebe544d50dc99d7d54ee7124934ee5b7d64053e
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-01-10 12:23:54 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (71.64%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int k=minutes;
-       // vector<int>&arr=customers;
-        int n=customers.size();
-        int prevLoss=0;
-        for(int i=0;i<k;i++){
-           if(grumpy[i]==1){
+       int k=minutes;
+       int n=customers.size();
+       int prevLoss=0;
+       for(int i=0;i<k;i++){
+         if(grumpy[i]==1){
             prevLoss+=customers[i];
-           }
+         }
+       } 
+       int maxLoss=prevLoss;
+       int maxIdx=0;
+       int j=k;
+       int i=1;
+       while(j<n){
+        int currLoss=prevLoss;
+        if(grumpy[j]==1) currLoss+=customers[j];
+        if(grumpy[i-1]==1)currLoss-=customers[i-1];
+        if(currLoss>maxLoss){
+            maxLoss=currLoss;
+            maxIdx=i;
         }
-        int maxLoss=prevLoss;
-        int maxIdx=0;
-        int j=k;
-        int i=1;
-        while(j<n){
-            int currLoss=prevLoss;
-            if(grumpy[j]==1)currLoss+=customers[j];
-            if(grumpy[i-1]==1)currLoss-=customers[i-1];
-            if(currLoss>maxLoss){
-                maxLoss=currLoss;
-                maxIdx=i;
-            }
-            i++;
-            j++;
-            prevLoss=currLoss;
+        i++;
+        j++;
+        prevLoss=currLoss;
+       }
+       for(int i=maxIdx;i<maxIdx+k;i++){
+         grumpy[i]=0;
+       }
+       int sum=0;
+       for(int i=0;i<n;i++){
+        if(grumpy[i]==0){
+            sum+=customers[i];
         }
-        for(int i=maxIdx;i<maxIdx+k;i++){
-             grumpy[i]=0;
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            if(grumpy[i]==0){
-                sum+=customers[i];
-            } 
-        }
-        return sum;
+       }
+       return sum;
     }
 };
