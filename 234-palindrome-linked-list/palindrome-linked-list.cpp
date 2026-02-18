8e096b3615f093fadaf3eca95de37d19f873dea6
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-02-18 22:48:08 +0530

    Time: 75 ms (5.03%) | Memory: 152.6 MB (5.47%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,37 +11,36 @@
 class Solution {
 public:
      ListNode*reverse(ListNode*head){
-       ListNode*prev=NULL;
-       ListNode*current=head;
-       ListNode*Next=head;
-       while(current!=NULL){
-        Next=current->next;
-        current->next=prev;
-        prev=current;
-        current=Next;
-       }
-       return prev;
+        ListNode*prev=NULL;
+        ListNode*curr=head;
+        ListNode*Next=head;
+        while(curr!=NULL){
+            Next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=Next;
+        }
+        return prev;
      }
     bool isPalindrome(ListNode* head) {
-       //ListNode*c=new ListNode(10);
-       //ListNode*temp=head;
-      // ListNode*tempc=c;
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        ListNode*c=new ListNode(100);
+        ListNode*tempc=c;
+        ListNode*temp=head;
+        while(temp!=NULL){
+           ListNode*t=new ListNode(temp->val);
+           tempc->next=t;
+           temp=temp->next;
+           tempc=tempc->next;
         }
-       //c=c->next;
-       ListNode*newNode=reverse(slow->next);
-      // slow->next=NULL;
-       ListNode*a=head;
-       ListNode*b=newNode;
-       while(b!=NULL){
-        if(a->val!=b->val)return false;
-        a=a->next;
-        b=b->next;
-       }
-       return true;
+        c=c->next;
+        c=reverse(c);
+        ListNode*a=head;
+        ListNode*b=c;
+        while(a!=NULL){
+            if(a->val!=b->val)return false;
+            a=a->next;
+            b=b->next;
+        }
+        return true;
     }
 };
