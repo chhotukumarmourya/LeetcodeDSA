71343c175083a4b119243826e2f9be4634123c42
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-04-02 15:53:28 +0530

    Time: 3 ms (57.05%) | Memory: 12.5 MB (92.91%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,55 +1,52 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
 class Solution {
 public:
-    int calculate(string s) {
-        int result = 0;      // current result
-        int sign = 1;        // current sign: 1 for '+', -1 for '-'
-        stack<int> st;       // stack for parentheses: stores (result_before, sign_before)
-        
-        int i = 0;
+    int calculate(std::string s) {
+        std::stack<int> st;
+        int current_result = 0;
+        int sign = 1; // 1 for '+', -1 for '-'
         int n = s.length();
-        
-        while (i < n) {
-            char ch = s[i];
-            
-            if (ch == ' ') {
-                i++;
-            } 
-            else if (ch == '+') {
-                sign = 1;
-                i++;
-            } 
-            else if (ch == '-') {
-                sign = -1;
-                i++;
-            } 
-            else if (ch == '(') {
-                // push current result and sign onto stack
-                st.push(result);
-                st.push(sign);
-                // reset for new sub-expression
-                result = 0;
-                sign = 1;
-                i++;
-            } 
-            else if (ch == ')') {
-                // pop saved sign and previous result
-                int prev_sign = st.top(); st.pop();
-                int prev_result = st.top(); st.pop();
-                // combine: prev_result + prev_sign * (current result)
-                result = prev_result + prev_sign * result;
-                i++;
-            } 
-            else { // digit
-                // parse the whole number
-                long long num = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+
+            if (isdigit(c)) {
+                long long num = 0; // Use long long to prevent overflow during parsing
                 while (i < n && isdigit(s[i])) {
                     num = num * 10 + (s[i] - '0');
                     i++;
                 }
-                result += sign * num;
+                current_result += sign * num;
+                i--; // Adjust for the outer loop increment
+            } 
+            else if (c == '+') {
+                sign = 1;
+            } 
+            else if (c == '-') {
+                sign = -1;
+            } 
+            else if (c == '(') {
+                // Push the current result and sign onto the stack
+                st.push(current_result);
+                st.push(sign);
+                // Reset for the new scope inside parentheses
+                current_result = 0;
+                sign = 1;
+            } 
+            else if (c == ')') {
+                // First pop the sign that was before the '('
+                int prev_sign = st.top();
+                st.pop();
+                // Then pop the result that was before the '('
+                int prev_result = st.top();
+                st.pop();
+
+                current_result = prev_result + (prev_sign * current_result);
             }
         }
-        
-        return result;
+        return current_result;
     }
 };
