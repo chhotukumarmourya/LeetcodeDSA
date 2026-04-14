b34f8e983f31350379bb38a053172c27b00624fb
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-04-14 11:45:20 +0530

    Time: 24 ms (54.10%) | Memory: 139.2 MB (43.13%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -8,7 +8,7 @@ public:
         for(int i=0;i<n;i++){
             while(dq.size()>0 && nums[i]>nums[dq.back()] )dq.pop_back();
             dq.push_back(i);
-            while(dq.front()<i-k+1)dq.pop_front();
+            if(dq.front()<i-k+1)dq.pop_front();
             if(i>=k-1)ans.push_back(nums[dq.front()]);
         }
         return ans;
