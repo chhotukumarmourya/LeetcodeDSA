ee5201e03ef07814331d3cad29522fde6af0d496
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-04-17 10:21:12 +0530

    Time: 63 ms (26.21%) | Memory: 23.9 MB (84.43%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,39 +9,39 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-       ListNode*tempA=headA;
-      ListNode*tempB=headB;
-      int lA=0;
-      int lB=0;
-      while(tempA!=NULL){
-        lA++;
-        tempA=tempA->next;
-      }
-      while(tempB!=NULL){
-        lB++;
-        tempB=tempB->next;
-      }
-      tempA=headA;
-      tempB=headB;
-      if(lA>lB){
-        int idx=lA-lB;
-        for(int i=1;i<idx+1;i++){
+        ListNode*tempA=headA;
+        ListNode*tempB=headB;
+        int lenA=0;
+        int lenB=0;
+         while(tempA!=NULL){
+            lenA++;
             tempA=tempA->next;
-        }while(tempA!=tempB){
+            
+         }
+         while(tempB!=NULL){
+            lenB++;
+            tempB=tempB->next;
+            
+         }
+         tempA=headA;
+         tempB=headB;
+         if(lenA>lenB){
+            int skip=lenA-lenB;
+            for(int i=0;i<skip;i++){
+                 tempA=tempA->next;
+            }
+         }else{
+            int skip=lenB-lenA;
+            for(int i=0;i<skip;i++){
+                tempB=tempB->next;
+            }
+         }
+         while(tempA !=NULL && tempB!=NULL){
+            if(tempA==tempB)return tempA;
             tempA=tempA->next;
             tempB=tempB->next;
-        }
-        return tempA;
-      }else{
-        int idx=lB-lA;
-        for(int i=1;i<idx+1;i++){
-            tempB=tempB->next;
-        }
-        while(tempB!=tempA){
-           tempA=tempA->next;
-            tempB=tempB->next; 
-        }
-        return tempA;
-      }
+         }
+         return NULL;
+
     }
 };
