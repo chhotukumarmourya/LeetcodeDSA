f9ac973ab35886777dc7ca52535b4669b686ae36
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-04-21 11:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (97.68%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -15,27 +15,20 @@ public:
         if(root==NULL)return 0;
         return 1+max(level(root->left),level(root->right));
      }
-     void nthlevel(TreeNode*root,int curr,int level,vector<int>&v){
-        if(root==NULL)return;
-        if(curr==level){
-           v.push_back(root->val);
-            return ;
-        }
-        nthlevel(root->left,curr+1,level,v);
-        nthlevel(root->right,curr+1,level,v);
-     }
-     void helper(TreeNode*root,vector<vector<int>> &ans){
-        int n=level(root);
-        for(int i=1;i<=n;i++){
-            vector<int>v;
-         nthlevel(root,1,i,v) ;
-         ans.push_back(v);
-            cout<<endl;
-        }
+     
+     void helper(TreeNode*root,vector<vector<int>> &ans,int level){
+        if(root==NULL)return ;
+        ans[level].push_back(root->val);
+        helper(root->left,ans,level+1);
+        helper(root->right,ans,level+1);
      }
     vector<vector<int>> levelOrder(TreeNode* root) {
+        int n=level(root);
         vector<vector<int>>ans;
-        helper(root,ans);
+        for(int i=0;i<n;i++){
+           ans.emplace_back(); 
+        }
+        helper(root,ans,0);
         return ans;
     }
 };
