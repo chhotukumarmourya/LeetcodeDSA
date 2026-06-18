7ad7413fae43971a14e21232254c38f67b373cbd
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-06-18 23:54:56 +0530

    Time: 176 ms (49.97%) | Memory: 193.9 MB (60.53%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,26 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    TreeNode*first=NULL;
-    void markParent(TreeNode*root,unordered_map<TreeNode*,TreeNode*>&parent){
-        if(root==NULL)return;
-        if(root->left!=NULL)parent[root->left]=root;
-        if(root->right!=NULL)parent[root->right]=root;
-        markParent(root->left,parent);
-        markParent(root->right,parent);
+     TreeNode*first=NULL;
+     void markparent(TreeNode*root,unordered_map<TreeNode*,TreeNode*>&parent){
+         if(root==NULL) return;
+         if(root->left!=NULL){
+            parent[root->left]=root;
+         }
+         if(root->right!=NULL){
+            parent[root->right]=root;
+         }
+          markparent(root->left,parent);
+         markparent(root->right,parent);
     }
     void find(TreeNode*root,int start){
-        if(root==NULL)return;
+        if(root==NULL)return ;
         if(root->val==start){
             first=root;
             return;
@@ -31,25 +25,25 @@ public:
     int amountOfTime(TreeNode* root, int start) {
         find(root,start);
         unordered_map<TreeNode*,TreeNode*>parent;
-        markParent(root,parent);
+        markparent(root,parent);
         unordered_set<TreeNode*>s;
         s.insert(first);
         queue<pair<TreeNode*,int>>q;
         q.push({first,0});
-        int maxLevel=0;
+        int maxlevel=0;
         while(q.size()>0){
             pair<TreeNode*,int>p=q.front();
             q.pop();
             int level=p.second;
-            maxLevel=max(maxLevel,level);
+            maxlevel=max(maxlevel,level);
             TreeNode*temp=p.first;
-            if(temp->left){
+            if(temp->left!=NULL){
                 if(s.find(temp->left)==s.end()){
                     q.push({temp->left,level+1});
                     s.insert(temp->left);
                 }
             }
-             if(temp->right){
+            if(temp->right!=NULL){
                 if(s.find(temp->right)==s.end()){
                     q.push({temp->right,level+1});
                     s.insert(temp->right);
@@ -61,8 +55,7 @@ public:
                     s.insert(parent[temp]);
                 }
             }
-            
-        } 
-        return maxLevel;
+        }
+        return maxlevel;
     }
 };
