d23e33302cac1ecc6941ae6ec74dd071f0629125
CHHOTU KUMAR <chhotukumarckraja@gmail.com>
2026-06-21 22:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (5.54%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<nums.size();i++){
+              pq.push({nums[i],i});
+        }
+        return pq.top().second;
+    }
+};
